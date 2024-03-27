b4ee09f4e9c4809c4a475906132538dc348783ca
anhvipro569 <162719805+anhvipro569@users.noreply.github.com>
2024-03-27 22:56:20 +0700

    Update LHH.lua

--- LHH.lua
+++ LHH.lua
@@ -13084,10 +13084,10 @@ end)
             StopTween(_G.Auto_Dungeon)
         end)
 
-spawn(function()
-    while wait() do
-            if _G.Auto_Dungeon then
-                if not game.Players.LocalPlayer.PlayerGui.Main.Timer.Visible == false then
+    spawn(function()
+        pcall(function()
+            while wait() do
+                if _G.Auto_Dungeon then
                     if game:GetService("Players")["LocalPlayer"].PlayerGui.Main.Timer.Visible == true then
                     if game:GetService("Workspace")["_WorldOrigin"].Locations:FindFirstChild("Island 5") then
                         topos(game:GetService("Workspace")["_WorldOrigin"].Locations:FindFirstChild("Island 5").CFrame*RaidPos)
